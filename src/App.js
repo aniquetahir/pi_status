584e7fcd93cf3d0bd6850105e08745d38442fd21
Anique Rogers Tahir <aniquetahir@gmail.com>
2018-10-24 00:38:55 -0400

    Completed interface mock

--- src/App.js
+++ src/App.js
@@ -2,24 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class EmailStatistics extends Component {
+  render(){
+    return (
+        <div className="uk-grid uk-grid-collapse uk-child-width-1-1">
+          <div><span uk-icon="mail"></span>10 unread</div>
+        </div>
+    );
+  }
+}
+
+class UpcomingEventsMenu extends Component {
+  render(){
+    return (
+        <table className="uk-table uk-table-striped" style={{marginTop: 0}}>
+            <caption><span className="uk-label uk-label-warning">Upcoming Calendar Events</span></caption>
+            <thead>
+            <tr>
+                <th className="uk-table-shrink uk-text-nowrap">Date/Time</th>
+                <th className="uk-table-expand">Title</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td className="uk-text-nowrap">Table Data</td>
+                <td className="uk-text-truncate">Lorem Ipsum Dolores Lorem Ipsum Dolores</td>
+            </tr>
+            <tr>
+                <td className="uk-text-nowrap">Table Data</td>
+                <td className="uk-text-truncate">Lorem Ipsum Dolores Lorem Ipsum Dolores</td>
+            </tr>
+            <tr>
+                <td className="uk-text-nowrap">Table Data</td>
+                <td className="uk-text-truncate">Lorem Ipsum Dolores Lorem Ipsum Dolores</td>
+            </tr>
+            </tbody>
+        </table>
+    );
+  }
+}
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="uk-container" style={{padding: 0}}>
+          <div className="uk-grid-collapse uk-child-width-1-2" uk-grid="">
+              <div><b>AniqueTech</b></div>
+              <div style={{textAlign: 'right'}}><span>ip:192.168.0.1</span></div>
+          </div>
+          <UpcomingEventsMenu/>
+          <EmailStatistics/>
       </div>
     );
   }
